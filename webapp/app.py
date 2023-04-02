225748ab2348e8649a22531fad8d637acc191572
teovonkesselstatt <54458556+teovonkesselstatt@users.noreply.github.com>
2023-04-02 20:28:56 -0300

    me dejo de joder con cosas fancy
    sera ineficiente pero funciona

--- webapp/app.py
+++ webapp/app.py
@@ -13,42 +13,4 @@ def run_app():
     selection = st.sidebar.radio("Go to", list(PAGES.keys()))
     page = PAGES[selection]
 
-    # Read the CSV file into a pandas dataframe
-    df = pd.read_csv('data/REER_annual.csv')
-
-    # Drop first columns
-    df = df.iloc[:, 2:]
-
-    # Turn into long dataframe, keeping first columns
-    df = pd.melt(df, id_vars = ['Country', 'Region'], value_vars=df.columns[2:], var_name='Year', value_name='REER')
-    df.set_index('Year', inplace = True)
-    df.index = pd.to_datetime(df.index)
-
-    # Change Areas
-    df['Area'] = df.loc[:, 'Region']
-    df.loc[(df['Region'].str.contains('South America|Central America|Caribbean')), 'Area'] = 'Latin America'
-    df.loc[(df['Region'].str.contains('Africa')), 'Area'] = 'Africa'
-    df.loc[(df['Region'].str.contains('South-eastern Asia|Southern Asia')), 'Area'] = 'South Asia'
-    df.loc[(df['Region'].str.contains('Western Asia|Central Asia')), 'Area'] = 'Middle East'
-    df.loc[(df['Region'].str.contains('Southern Europe|Western Europe|Northern Europe')), 'Area'] = 'Western Europe'
-    df.loc[(df['Region'].str.contains('Melanesia|Polynesia')), 'Area'] = 'Pacific Islands'
-    df.loc[(df['Country'].str.contains('Russia')), 'Area'] = 'Russia'
-    df.loc[(df['Region'].str.contains('Northern America')), 'Area'] = 'Aamerica'
-
-
-    # Hago que Area sea la segunda columna
-    column_to_move = df.pop("Area")
-    df.insert(1, "Area", column_to_move)
-
-    # Get list of areas
-    areas = df.Area.unique()
-
-    # Make numeric values floats
-    df.iloc[:, 3:].astype(np.float64)
-
-    # Calculate percentage change column
-    df_shifted = df[['Country','REER']].groupby('Country').shift(1)
-    df['Var'] = ((df['REER'] - df_shifted['REER']) / df_shifted['REER'])
-
-
-    page.run_app(df)
+    page.run_app()
